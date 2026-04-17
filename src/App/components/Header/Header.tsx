2d8ae4fe8a427e22d88aa79202e764465da0f849
d1n8 <mr.dinyar@mail.ru>
2026-04-18 00:34:14 +0500

    add header

--- src/App/components/Header/Header.tsx
+++ src/App/components/Header/Header.tsx
@@ -1,8 +1,17 @@
+import LogoIcon from 'components/Icons/Logo';
 import styles from './Header.module.scss'
+import BellIcon from 'components/Icons/Bell';
+import UserIcon from 'components/Icons/User';
 
 function Header() {
     return ( 
-        <header className={styles.header}>header</header>
+        <header className={styles.header}>
+            <LogoIcon/>
+            <nav className={styles.nav}>
+                <BellIcon/>
+                <UserIcon/>
+            </nav>
+        </header>
      );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { routesConfig } from "./config/routesConfig";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import 'config/configureMobX'
+import 'styles/index.scss'
 
 const router = createBrowserRouter(routesConfig);
 
