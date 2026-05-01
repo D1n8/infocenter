6d1068a8f750093cdb7a44e71f023c57cfbbbb17
d1n8 <mr.dinyar@mail.ru>
2026-05-01 15:52:03 +0500

    fix/fix problem with routing and url

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,9 @@ import { routesConfig } from './config/routesConfig';
 import 'config/configureMobX';
 import 'styles/index.scss';
 
-const router = createBrowserRouter(routesConfig);
+const router = createBrowserRouter(routesConfig, {
+  basename: import.meta.env.BASE_URL,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement);
 
